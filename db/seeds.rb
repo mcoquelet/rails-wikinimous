b4d01a3e762fb41024c7dfdf423da4b91191d5fb
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-01-14 16:41:55 +0100

    add rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Article.create!(title: Faker::Book.title, content: Faker::Lorem.paragraph_by_chars(number: 5000, supplemental: false))
+end
